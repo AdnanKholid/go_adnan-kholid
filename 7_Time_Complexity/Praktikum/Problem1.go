e12c43a2d263f55fed94f72e66c2f61433204dd9
Adnan Kholid <adnankholid@gmail.com>
2022-09-01 16:14:02 +0700

    Tugas Praktikum

---
+++ 7_Time_Complexity/Praktikum/Problem1.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func primeNumber(number int) bool {
+	for i := 2; i <= number; i++ {
+		if number%i == 0 {
+			if number == i {
+				return true
+			} else {
+				return false
+			}
+		}
+	}
+
+	return false
+}
+
+func main() {
+
+	fmt.Println(primeNumber(1000000007))
+
+	fmt.Println(primeNumber(13))
+
+	fmt.Println(primeNumber(17))
+
+	fmt.Println(primeNumber(20))
+
+	fmt.Println(primeNumber(35))
+}
